309387bbd36f5012796b800c3372415b93d673d8
JarandDev <jarand@jarand.dev>
2021-07-08 15:17:06 +0200

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import i18next from "i18next";
 import {I18nextProvider} from "react-i18next";
 import common_en from "./translations/en/common.json";
@@ -37,8 +36,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
--- src/redirectFunctions.js
+++
@@ -1,5 +0,0 @@
-function isValidRedirectUrl(redirectUrl) {
-    return /https:\/\/[a-z0-9-]*[.]*jarand\.dev/g.test(redirectUrl);
-}
-
-export {isValidRedirectUrl};
